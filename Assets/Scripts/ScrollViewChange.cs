7188fa429b4db1993ca46f349672ce95af4c6979
wchang98 <47240838+wchang98@users.noreply.github.com>
2021-11-17 21:14:37 +0900

    스크롤뷰 높이 자동설정 (보완필요)

---
+++ Assets/Scripts/ScrollViewChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewChange : MonoBehaviour
+{
+    public GameObject scrollView;
+    GameObject content;
+    RectTransform rectTransform;
+    Text text;
+
+    float enterSize;
+
+    private void Start()
+    {
+        content = gameObject;
+        text = content.GetComponent<Text>();
+        rectTransform = content.GetComponent<RectTransform>();
+        enterSize = scrollView.GetComponent<RectTransform>().rect.height / (100f / text.fontSize);
+    }
+
+
+    void Update()
+    {
+        Rect rect = rectTransform.rect;
+        rect.height = text.text.Split('\n').Length * enterSize;
+        rectTransform.sizeDelta = new Vector2(0, rect.height);
+    }
+}
