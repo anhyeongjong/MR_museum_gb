cbaf9b0e6e48cf2a1450ca682e81701ae9c98c92
wchang98 <47240838+wchang98@users.noreply.github.com>
2021-11-18 09:48:18 +0900

    스크롤뷰 높이 자동설정
    * ScrollViewChange.cs
    - ScrollUp, ScrollDown 함수 참조해서 사용 시 1줄씩 이동 가능

--- Assets/Scripts/ScrollViewChange.cs
+++ Assets/Scripts/ScrollViewChange.cs
@@ -7,24 +7,53 @@ public class ScrollViewChange : MonoBehaviour
 {
     public GameObject scrollView;
     GameObject content;
-    RectTransform rectTransform;
+    RectTransform contentRectTransform, scrollRecttransform;
     Text text;
 
-    float enterSize;
+    float enterSize, lineValue;
 
     private void Start()
     {
         content = gameObject;
         text = content.GetComponent<Text>();
-        rectTransform = content.GetComponent<RectTransform>();
-        enterSize = scrollView.GetComponent<RectTransform>().rect.height / (100f / text.fontSize);
+        contentRectTransform = content.GetComponent<RectTransform>();
+        scrollRecttransform = scrollView.GetComponent<RectTransform>();
+
+        lineValue = (scrollRecttransform.rect.height / text.fontSize / 1.15f);
+        enterSize = scrollRecttransform.rect.height / lineValue;
+    }
+
+    void SizeChange()
+    {
+        Rect rect = contentRectTransform.rect;
+        rect.height = text.text.Split('\n').Length * enterSize;
+        contentRectTransform.sizeDelta = new Vector2(0, rect.height);
+
+        //Debug.Log($"ScrollHeight : {scrollRecttransform.rect.height}, ContentHeight : {rect.height}, lineValue : {lineValue}");
     }
 
 
+    [ContextMenu("스크롤올리기")]
+    public void ScrollUp()
+    {
+        ScrollMove(-enterSize);
+    }
+
+    [ContextMenu("스크롤내리기")]
+    public void ScrollDown()
+    {
+        ScrollMove(enterSize);
+    }
+
+    public void ScrollMove(float f)
+    {
+        Vector2 position = contentRectTransform.anchoredPosition;
+        position.y += f;
+        contentRectTransform.anchoredPosition = position;
+    }
+
     void Update()
     {
-        Rect rect = rectTransform.rect;
-        rect.height = text.text.Split('\n').Length * enterSize;
-        rectTransform.sizeDelta = new Vector2(0, rect.height);
+        SizeChange();
     }
 }
