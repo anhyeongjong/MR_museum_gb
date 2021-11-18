cd7367f1745b0eca138796be336552e2ed03fef2
rech4210 <65288322+rech4210@users.noreply.github.com>
2021-11-18 19:33:24 +0900

    박물관모델링 배치
    무령왕릉비, 종 크기작업
    조명작업 (하이라이트)
    박물관 씬 추가

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -121,6 +121,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Center
+        {
+            get
+            {
+                return SteamVR_Actions.default_Center;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
