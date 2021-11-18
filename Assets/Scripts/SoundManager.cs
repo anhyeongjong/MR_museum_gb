0e227a66096945eafb6df769ed167e8ebe463852
wchang98 <47240838+wchang98@users.noreply.github.com>
2021-11-18 18:34:38 +0900

    사운드 및 유물 정보 표기를 위한 작업
    * Drone Asset 추가
    
    * SoundManager.cs, LegacyData.cs 추가

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void AudioPlay(AudioClip clip)
+    {
+        if(audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+
+        audioSource.clip = clip;
+
+        audioSource.PlayDelayed(1f);
+    }
+
+    public void AudioStop()
+    {
+        audioSource.Stop();
+    }
+}
